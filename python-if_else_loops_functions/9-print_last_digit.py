1f561564bcec001f7f3b52619f123ff5e93288f7
Kiko Sprague <ksprague0592@gmail.com>
2024-05-07 14:22:09 -0500

    forgot double quotes in print

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-          print(f{number % -10})
+          print(f"{umber % -10}")
           return(number % -10)
     else:
-         print (f{number % 10})
+         print (f"{number % 10}")
          return(number % 10)
