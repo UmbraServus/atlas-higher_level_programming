aba687417477406e0226bcc13ab37efedebe6b5e
Kiko Sprague <ksprague0592@gmail.com>
2024-05-07 23:32:06 -0500

    idk

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print("{:d}".format(col))
-            if row != col[-1]:
+            if col != len(matrix[row]) - 1:
                 print(" ", end="")
         print()
