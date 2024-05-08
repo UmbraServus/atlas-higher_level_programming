8ad4289e0a158e4f15aea48b1757712c9d431eaf
Kiko Sprague <ksprague0592@gmail.com>
2024-05-07 19:11:58 -0500

    fixed over indentation and a whitespace issue after index +=

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
-            print("1 argument:\n1: {}".format(sys.argv[1]))
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in sys.argv[1:]:
             print("{}: {}".format(index, i))
-            index+= 1
+            index += 1
