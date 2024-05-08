fd7ae362453d82a28a945ac825a4607b397fafec
Kiko Sprague <ksprague0592@gmail.com>
2024-05-07 23:23:47 -0500

    added if statement to add in space between numbers without adding extra chars for checker.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print("{:d} ".format(col), end="")
+            print("{:d}".format(col))
+            if row != col[-1]:
+                print(" ", end="")
         print()
