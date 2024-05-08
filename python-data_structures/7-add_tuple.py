4629289915ec5185dd9d6ddd467c6275a9e00a25
Kiko Sprague <ksprague0592@gmail.com>
2024-05-08 00:01:04 -0500

    forgot )

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = ((tuple_a[0] if len_a >= 1 else 0) +
                (tuple_b[0] if len_b >= 1 else 0),
                (tuple_a[1] if len_a >= 2 else 0) +
-               (tuple_b[2] if len_b >= 2 else 0)
+               (tuple_b[2] if len_b >= 2 else 0))
             
     return tuple_c
