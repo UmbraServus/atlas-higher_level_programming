0df5e7ea4be3ffd59a12d2db117eb1fd66157ee0
Kiko Sprague <ksprague0592@gmail.com>
2024-05-08 20:31:33 -0500

    finds the maximum value and returns the key to that value.

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    result = 0
+    tmp_key = None
+    for key, value in a_dictionary.items():
+        if value > result:
+            result = value
+            tmp_key = key
+    return tmp_key
