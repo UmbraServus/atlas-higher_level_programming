6e2486d7ead58aba9f4563fae78905b82a271a78
Kiko Sprague <ksprague0592@gmail.com>
2024-05-08 20:33:30 -0500

    added a check to see if dictionary is none.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     result = 0
     tmp_key = None
+    if not a_dictionary:
+        return None
     for key, value in a_dictionary.items():
         if value > result:
             result = value
