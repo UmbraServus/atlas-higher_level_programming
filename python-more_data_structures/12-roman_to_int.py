3db0699e262e2c49beba1cf1c7a4a4a99216b658
Kiko Sprague <ksprague0592@gmail.com>
2024-05-08 21:11:57 -0500

    had to take out numerals cause it was too long fro checker

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [numerals[x] for x in roman_string]
+    number = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [number[x] for x in roman_string]
     result = 0
     for y in range(len(numbers) - 1):
         if numbers[y] >= numbers[y + 1]:
