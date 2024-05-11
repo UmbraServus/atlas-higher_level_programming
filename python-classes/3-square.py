4ef11b3f512bba7ed29be8adee22e3ef4e68ea54
Kiko Sprague <ksprague0592@gmail.com>
2024-05-11 15:17:32 -0500

    square with area method in it

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module for square class """
+
+
+class Square:
+    """ square class """
+    def __init__(self, size=0):
+        """initialze square
+        Args:
+            size (int): size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns area of a square"""
+        return self.__size * self.__size
+    
