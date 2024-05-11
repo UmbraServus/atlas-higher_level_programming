348528c256c0b9437ace06ce323440b388638240
Kiko Sprague <ksprague0592@gmail.com>
2024-05-11 15:50:58 -0500

    I had area indented too much

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -25,6 +25,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
         
-        def area(self):
-            """returns area of a square"""
-            return self.__size * self.__size
+    def area(self):
+        """returns area of a square"""
+        return self.__size * self.__size
