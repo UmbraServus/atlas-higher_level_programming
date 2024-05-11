67325e6af6308d77b4e15cf122fc7654ddc4c9a2
Kiko Sprague <ksprague0592@gmail.com>
2024-05-11 15:33:22 -0500

    added a getter/setter to the code

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" module for square class """
+
+
+class Square:
+    """ square class """
+    def __init__(self, size=0):
+        """initialze square
+        Args:
+            size (int): size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns size of the square instance
+            setter sets the size of the square instance"""
+        return self.__size
+    
+    @size.getter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+        def area(self):
+            """returns area of a square"""
+            return self.__size * self.__size
