14956e9ced183acace981276426bb95db4687913
Kiko Sprague <ksprague0592@gmail.com>
2024-05-14 18:22:26 -0500

    forgot an underscore on the width variable in the __repr__ module

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
         return rectangle
 
     def __repr__(self):
-        return f"Rectangle({self._width}, {self.__height})"
+        return f"Rectangle({self.__width}, {self.__height})"
