b88cc6546124aab5d70b6a76f37002eef1e2dd09
Kiko Sprague <ksprague0592@gmail.com>
2024-05-14 16:06:00 -0500

    took out elif and else statments.

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Definea rectangle class"""
+"""Define a rectangle class"""
 
 
 class Rectangle:
@@ -25,10 +25,9 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -39,7 +38,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must but >= 0")
-        else:
-            self.__height = value
+        self.__height = value
