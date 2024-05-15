3a7f78c83a3d1d60e7c2ed1a39b584ddfb3ec62f
Kiko Sprague <ksprague0592@gmail.com>
2024-05-14 21:41:14 -0500

    accidentally had the syntax wrong in the if statements that raise errors.

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -85,13 +85,18 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if rect_1 is not isinstance(rect_1, Rectangle):
+        """checks if rect 1 is bigger or equal to rect 2
+            Args:
+                rect_1: rectangle one
+                rect_2: rectangle two
+            Raise:
+                TypeError: if rect1 or rect2 are not a Rectangle type
+        """
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_2 is not isinstance(rect_2, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_2.area() > rect_1.area():
-            return rect_2
         else:
-            return rect_1
+            return rect_2
