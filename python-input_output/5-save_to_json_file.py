d6fbfecb5c9ee0e06ddc77a7ebac3461057ce8f5
Kiko Sprague <ksprague0592@gmail.com>
2024-05-21 23:50:55 -0500

    creates obj from JSON file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,6 @@
 def save_to_json_file(my_obj, filename):
     """saves an json obj to file as a string"""
 
-
     with open(filename, 'w', encoding="utf-8") as f:
         import json
         f.write(json.dumps(my_obj))
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""load from json file module"""
+
+
+def load_from_json_file(filename):
+    """creates and obj from json file"""
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        import json
+        return json.loads(f)
