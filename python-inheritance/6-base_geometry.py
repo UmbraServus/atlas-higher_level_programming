4bef40e7abf0b812fe903a6369a3b3a81262e613
Kiko Sprague <ksprague0592@gmail.com>
2024-05-21 20:30:03 -0500

    saved 6- as 5- by accident

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" BaseGeometry class and methods """
+
+
+class BaseGeometry():
+    """ empty class """
+
+    def area(self):
+        """ not implemented yet """
+        raise Exception("area() is not implemented")
+    
