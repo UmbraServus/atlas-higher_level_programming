b5ddce1ffe96d28a1785aac47c8c6660063aae42
Kiko Sprague <ksprague0592@gmail.com>
2024-05-21 22:53:01 -0500

    writes to a file creates a new one if file doesnt exist.

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """opens and writes to a file
+
+        args:
+            filename: name of file
+            text: text to be added to file
+    """
+
+    
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
