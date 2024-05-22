c07d2c9a22c8724390e6bc952db22f700a6747fd
Kiko Sprague <ksprague0592@gmail.com>
2024-05-21 22:07:50 -0500

    9-rectangle implement area and set __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module documentation """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class with inheritance from BaseGeometry
+
+        Methods:
+            Area(): Not implemented yet
+            Integer_validator(): validates if given value is an integer
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        area = self.__width * self.__height
+        return area
+
+    def __str__(self):
+        return "[{}] {}/{}".format(type(self).__name__, self.__width,
+                                   self.__height)
