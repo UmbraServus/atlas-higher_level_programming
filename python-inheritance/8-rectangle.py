c0ac956c9eba113d799a006e2aefa5717eecf549
Kiko Sprague <ksprague0592@gmail.com>
2024-05-21 21:34:25 -0500

    rectangle class with inheritance from basegeometry class

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class with inheritance from BaseGeometry
+
+        Methods:
+            Area(): Not implemented yet
+            Integer_validator(): validates if given value is an integer
+    """
+    def __init__(self, height, width):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
