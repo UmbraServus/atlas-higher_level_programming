43adcc4885e5c4b05d67b834e441094a3998c98a
Kiko Sprague <ksprague0592@gmail.com>
2024-05-21 23:33:15 -0500

    not enough newlines between documentation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """save to json file module"""
 
+
 def save_to_json_file(my_obj, filename):
     """saves an json obj to file as a string"""
+
 
     with open(filename, 'w', encoding="utf-8") as f:
         import json
