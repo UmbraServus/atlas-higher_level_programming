90cf41d15704313ffce2043171f89656fe4e2155
Kiko Sprague <ksprague0592@gmail.com>
2024-05-25 16:38:08 -0500

    added a __str__ override to have it display the subclass the ID, x, y , width, and height.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -55,6 +55,9 @@ class Rectangle(Base):
                 print("#", end="")
             print("")
 
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
     @property
     def width(self):
         return self.__width
