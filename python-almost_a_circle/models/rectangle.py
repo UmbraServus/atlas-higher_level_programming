ed497d81d4ae6beea1f9f0c22bfd5e742bf696af
Kiko Sprague <ksprague0592@gmail.com>
2024-05-25 18:05:20 -0500

    started out with a for loops but it wasnt working until I realized I didnt need it and I should of jsut checked if args was not none and > than 0.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -65,6 +65,19 @@ class Rectangle(Base):
             f"{self.__width}/{self.__height}"
         )
 
+    def update(self, *args):
+        if args and len(args) > 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+
     @property
     def width(self):
         return self.__width
