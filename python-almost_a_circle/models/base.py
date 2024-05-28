e0058f2a6aece530e3553e7213bf974d86df6bd8
Kiko Sprague <ksprague0592@gmail.com>
2024-05-28 13:56:42 -0500

    trailing white space at 35:1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -32,7 +32,7 @@ class Base:
             return json.loads(json_string)
         else:
             return []
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """ writes the json str representation to classname.json """
