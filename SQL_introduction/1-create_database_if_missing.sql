989fbb0a51d6fe987acd8836885d450adeec44e2
UmbraServus <ksprague0592@gmail.com>
2024-06-14 02:49:01 -0500

    forgot comments

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,4 @@
-    CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- creates database if it doesnt exist
+-- creates a database if it doesnt exist because if it did you wouldnt need a dupe
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
