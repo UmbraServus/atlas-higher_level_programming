b69492f4e9a26bd3afd7d6948088bedca9a555bf
UmbraServus <ksprague0592@gmail.com>
2024-06-19 15:45:53 -0500

    -- creates database and table named cities -- id is primary key making it unique and auto increments. -- state_id is foreign key and refrences states

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,11 @@
--- creates database for states and table named states 
+-- creates database and table named cities 
 -- id is primary key making it unique and auto increments.
+-- state_id is foreign key and refrences states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL
+    FOREIGN KEY (state_id) REFERENCES states(id)
     name VARCHAR(256) NOT NULL
 );
