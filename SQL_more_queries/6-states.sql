f77b5715ec18d67cf2f057c7da577ff739cbf123
UmbraServus <ksprague0592@gmail.com>
2024-06-19 15:30:42 -0500

    added use database syntax

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates database for states and table named states 
 -- id is primary key making it unique and auto increments.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
