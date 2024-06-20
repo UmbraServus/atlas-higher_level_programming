b7b3a50080e6ee58d62febf6b54659ef867bea7d
UmbraServus <ksprague0592@gmail.com>
2024-06-19 22:02:06 -0500

    forgot to add number_ofshows to the order by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ AS genre, count(show_id)
 AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre ORDER BY DESC
+GROUP BY genre ORDER BY number_of_shows DESC;
