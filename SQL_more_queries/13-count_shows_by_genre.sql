fd6ad43ffaa7f28b1e44f01eee4d5485a1359dea
UmbraServus <ksprague0592@gmail.com>
2024-06-19 21:58:41 -0500

    lists genre and count of shows in the genre as long as they have a show.

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists genre and count of shows in the genre as long as they have a show. 
+SELECT DISTINCT name
+AS genre, count(show_id)
+AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre ORDER BY DESC
