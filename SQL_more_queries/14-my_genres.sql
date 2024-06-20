370cd48e4f342c871e6903577ae0d04abd60a2bd
UmbraServus <ksprague0592@gmail.com>
2024-06-19 22:13:47 -0500

    -- lists tv show genres of dexter by joinning tv show genres -- to tv genres then tv shows to tv show genre also.

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- lists tv show genres of dexter by joinning tv show genres
+-- to tv genres then tv shows to tv show genre also.
+SELECT name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id     
+INNER JOIN tv_shows ON tv_show_genres.show_id =  tv_shows.id 
+WHERE title = 'Dexter'
+ORDER BY name ASC;
