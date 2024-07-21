540c9692ea5d6679822f9ed952b467d9eb9a75a2
UmbraServus <ksprague0592@gmail.com>
2024-07-21 02:12:26 -0500

    took out .length

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+if (!process.argv[2]) {
     console.log("No argument");
-} else if (process.argv.length < 4) {
-    console.log(process.argv[2])
 } else {
     for (let i = 2; i < process.argv.length; i++) {
         console.log(process.argv[i]);
