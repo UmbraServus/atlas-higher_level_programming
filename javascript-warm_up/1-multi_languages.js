24176ebb696c8caa44bfdfff1edc4a2da5093bc8
UmbraServus <ksprague0592@gmail.com>
2024-07-21 02:14:55 -0500

    Readme and some types in 1 an 0

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 
 const var1 = "C is fun"
 const var2 = "Python is cool"
-const var3 = "Javascript is amazing"
+const var3 = "JavaScript is amazing"
 
 console.log(var1 + "\n" + var2 + "\n" + var3);
