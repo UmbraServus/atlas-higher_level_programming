3d2ef9787a859cc88d89166176aceb133ad0c881
UmbraServus <ksprague0592@gmail.com>
2024-07-21 02:18:48 -0500

    changed to whiile loop since I dont have a clear idea when the i needs to stop.

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,7 +3,9 @@
 if (!process.argv[2]) {
     console.log("No argument");
 } else {
-    for (let i = 2; i < process.argv.length; i++) {
+    let i = 2
+    while (process.argv[i] !== undefined) {
         console.log(process.argv[i]);
+        i++
     }
 }
