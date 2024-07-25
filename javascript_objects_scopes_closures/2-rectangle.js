4597c9491b720babad17a0ebad4fd9e9bb610436
UmbraServus <ksprague0592@gmail.com>
2024-07-24 17:30:32 -1000

    added files up to square; finished files up to 3-rectangle and fixed some semicolon and documenation issues.

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            return {}
-        }
-        else {
-            this.width = w;
-            this.height = h;
-        }
+  constructor(w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+  print() {
+    let rectangle = ''
+        for (let x = 0; x < this.height; x++) {
+        for (let i = 0; i < this.width; i++) {
+            rectangle += 'X';
+        }
+        if (x < this.height - 1) {
+            rectangle += '\n';
+        }
+    }
+    console.log(rectangle);
+  }
+
+}
+
+module.exports = Rectangle;
