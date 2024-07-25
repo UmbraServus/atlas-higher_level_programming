480b8dedcc1407595809640e489a592ddc5e5488
UmbraServus <ksprague0592@gmail.com>
2024-07-24 16:11:36 -1000

    fixed some indentation

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 if (!process.argv[2]) {
-    console.log("No argument");
+  console.log("No argument");
 } else {
-    let i = 2
-    while (process.argv[i] !== undefined) {
-        console.log(process.argv[i]);
-        i++
-    }
+  let i = 2
+  while (process.argv[i] !== undefined) {
+    console.log(process.argv[i]);
+    i++
+  }
 }
