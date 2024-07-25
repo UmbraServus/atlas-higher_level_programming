8da4b548324240a7e5de400e6db61c1d437c8758
UmbraServus <ksprague0592@gmail.com>
2024-07-24 16:51:39 -1000

    adding more complexities to the rectangle class by making sure the number isnt 0 or negative and also is an int

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,10 +2,14 @@
 
 class Rectangle {
   constructor(w, h) {
-    this.width = w;
-    this.height = h;
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}
+    }
+    else {
+      this.width = w;
+      this.height = h;
+    }
   }
-
 }
 
 module.exports = Rectangle;
