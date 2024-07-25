1d1b973aa3a0f6906d843350d53048cec3d12c46
UmbraServus <ksprague0592@gmail.com>
2024-07-24 18:09:21 -1000

    fixed spaces between parenthesis in  3-

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w >= 0 && h >= 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     let rectangle = '';
     for (let rows = 0; rows < this.height; rows++) {
       for (let columns = 0; columns < this.width; columns++) {
--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 class Square extends Rectangle {
-    
+    constructor(size) {
+        super(size, size);
+    }
 }
+
+module.exports = Square;
