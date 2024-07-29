7aa8b1083498baef16d23ece9c3012b8b24c3f2c
UmbraServus <ksprague0592@gmail.com>
2024-07-29 13:40:37 -1000

    documnetation error missing some semicolons and commented out a few unnessecary lines.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 script that reads a file and prints the data to std out.
 */
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
 const filePath = process.argv[2];
 
@@ -12,7 +12,7 @@ if (!filePath) {
   process.exit(1);
 }
 
-//const absolutePath = path.resolve(filePath);
+// const absolutePath = path.resolve(filePath);
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
@@ -21,4 +21,4 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   }
 
   console.log(data);
-})
+});
