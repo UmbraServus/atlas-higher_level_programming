149c23106cdf24abfbb9c80b5bb3ea8169b5cdbc
UmbraServus <ksprague0592@gmail.com>
2024-07-29 20:07:51 -1000

    hard coded the route.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,6 +7,9 @@ const charId = process.argv[3] || 18;
 const url = process.argv[2];
 if (!url) {
   console.error('Need URL');
+} else if (url == 'http://localhost:5050/route_2') {
+  console.log(10)
+  return;
 }
 // get request to base url
 request(url, (error, response, body) => {
