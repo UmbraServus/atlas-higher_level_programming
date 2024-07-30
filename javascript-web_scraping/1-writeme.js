3a03cf0b44a90ef1aa99b12ee34eadfd389c38f1
UmbraServus <ksprague0592@gmail.com>
2024-07-29 14:00:15 -1000

    fixed some documentation errors semicolons, spaces, variable name lowercase then uppercase, ect; commented out a console log that just let me know write was successful

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,17 +5,17 @@ simple use of the writefile function in fs module.
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const str_data = process.argv[3];
+const strData = process.argv[3];
 
-if (!filePath || !str_data) {
+if (!filePath || !strData) {
   console.error('Not enough arguments');
   process.exit(1);
 }
 
-fs.writeFile(filePath, str_data, 'utf8', (err) => {
+fs.writeFile(filePath, strData, 'utf8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log('File write was successful');
-})
+  // console.log('File write was successful');
+});
