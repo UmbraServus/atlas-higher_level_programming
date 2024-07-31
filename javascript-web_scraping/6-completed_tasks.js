28e893ef90016a7e819cf0f741d9b4ed80f1a651
UmbraServus <ksprague0592@gmail.com>
2024-07-30 18:41:05 -1000

    added 6-completed_tasks.js to webscraping using request and then iterating thru each task and checking if completed is true and adding to the right userid if so.

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+// get data
+request(url, (error, response, body) => {
+  if (error) {
+    console.error("Request error:", error);
+    process.exit(1);
+  }
+  //parse data
+  const tasks = JSON.parse(body);
+
+  // setup an obj to store tasks count and ids
+  const completedTasksCount = {};
+
+  // iterate through tasks
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (!completedTasksCount[task.userId]) {
+        completedTasksCount[task.userId] = 0;
+      }
+      completedTasksCount[task.userId]++;
+    }
+  });
+
+  // print task and user id
+  console.log(completedTasksCount);
+});
